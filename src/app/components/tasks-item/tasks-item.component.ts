5c3196f6b9464c2861ff203adc6464aaea12ec8a
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-06-29 14:04:12 +0530

    feat: Add a delete font to our app and style it, used fontawesome library for that. Add detailed notes on creating components, adding properties, events, and using directives
    - Detailed notes on creating components using Angular CLI
    - Explanation on adding properties to components and binding them to templates
    - Guide on adding and handling events in Angular components
    - In-depth coverage of using directives, including ngIf, ngFor, and ngStyle
    - Example of component integration using @Input decorator

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-tasks-item',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, NgStyle],
   templateUrl: './tasks-item.component.html',
-  styleUrl: './tasks-item.component.css'
+  styleUrls: ['./tasks-item.component.css'] 
 })
 export class TasksItemComponent {
   @Input() task: Task | undefined;
+  faTimes = faTimes;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faTimes);
+  }
 }
